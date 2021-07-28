9f71988a47d4917f3f9f8e5119ba82b687e7147e
Dispersioned <dispersioned@gmail.com>
2021-07-28 15:07:22 +0300

    add: header running line, works, footer

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,9 +1,11 @@
 let burgerIcon = document.querySelector('.header__burger');
 let burgerBody = document.querySelector('.header__nav');
 
-burgerIcon.addEventListener('click', function (e) {
-	document.querySelector('body').classList.toggle('_lock');
+if (burgerIcon) {
+	burgerIcon.addEventListener('click', function (e) {
+		document.querySelector('body').classList.toggle('_lock');
 
-	burgerIcon.classList.toggle('_active');
-	burgerBody.classList.toggle('_active');
-});
+		burgerIcon.classList.toggle('_active');
+		burgerBody.classList.toggle('_active');
+	});
+}
