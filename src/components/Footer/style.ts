7a9d64047a5b9465d623c4f351ab47fc78f359f9
Dispersioned <dispersioned@gmail.com>
2022-03-02 18:52:05 +0300

    add footer

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  margin-top: auto;
+`
+
+export const Body = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+  font-family: 'ABeeZee';
+`
