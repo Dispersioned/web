8d7a044e94544559ab0d63a4b8209137cdea3c55
Dispersioned <dispersioned@gmail.com>
2022-03-02 18:41:22 +0300

    add home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import Container from '../../components/Container/Container'
+import Header from '../../components/Header/Header'
+import { PageContent } from '../../style/global'
 
 const Home: React.FC = () => {
-  return <div>homepage</div>
+  return (
+    <Container>
+      <Header />
+      <PageContent>
+        <h1 style={{fontWeight: 400}}>Hi, I’m Max. A student frontend developer </h1>
+        <p>So far there is no important information here, yet you can see my projects.</p>
+      </PageContent>
+    </Container>
+  )
 }
 export default Home
