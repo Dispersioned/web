7fa42cb95683463241369ecb7dafcbaa0fdb2a76
Dispersioned <dispersioned@gmail.com>
2022-03-03 22:48:02 +0300

    prepare project page template

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './assets/fonts/index'
 import GlobalStyle from './style/global'
 import Home from './views/Home/Home'
+import Projects from './views/Projects/Projects'
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/projects" element={} />
-        <Route path="/experience" element={} />
+          <Route path="/projects" element={<Projects />} />
+          {/* <Route path="/experience" element={} />
         <Route path="/bio" element={} /> */}
           <Route path="/*" element={<Navigate replace to="/" />} />
         </Routes>
---
+++ src/views/Projects/Projects.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from '../../components/Container/Container'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import { Wrapper } from '../../style/global'
+
+const Projects: React.FC = () => {
+  return (
+    <Wrapper>
+      <Header />
+      <Container>
+        nothin here yet, Ha!
+      </Container>
+      <Footer />
+    </Wrapper>
+  )
+}
+export default Projects
