4c133ce23b59b8fdfd642b930c3fb978eaeb7f6a
Dispersioned <dispersioned@gmail.com>
2022-03-04 16:47:00 +0300

    commit to squash

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/img/logo.svg'
 import Container from '../Container/Container'
+import BurgerIcon from './BurgerIcon/BurgerIcon'
 import * as S from './style'
 
 const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleBurger = (): void => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <S.Header>
       <Container>
@@ -17,6 +24,9 @@ const Header: React.FC = () => {
             <S.Navlink to="/experience">Work Experience</S.Navlink>
             <S.Navlink to="/bio">Bio</S.Navlink>
           </S.Navbar>
+          <div onClick={handleBurger}>
+            <BurgerIcon className={isOpen ? 'active' : ''} />
+          </div>
         </S.Body>
       </Container>
     </S.Header>
