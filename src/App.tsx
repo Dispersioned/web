69d3bff3fa7a43077c8c4a78c5f37c0a2a0f47b5
Dispersioned <dispersioned@gmail.com>
2022-06-20 22:31:49 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './assets/fonts/index'
 import GlobalStyle from './style/global'
+import theme from './style/theme'
 import Home from './views/Home/Home'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
@@ -14,7 +16,7 @@ function App() {
           <Route path="/*" element={<Navigate replace to="/" />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/style/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Lora', '-apple-system', 'BlinkMacSystemFont', 'Arial', 'sans-serif'].join(','),
+  },
+})
+
+export default theme
