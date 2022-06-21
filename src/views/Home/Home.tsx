3db24bc3d5f9519b040764ee86b3015772f9ec78
Dispersioned <dispersioned@gmail.com>
2022-06-21 16:13:35 +0300

    add extra columns for data

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -9,20 +9,26 @@ const Home: React.FC = () => {
 
   return (
     <Container maxWidth="lg">
-      <Grid
-        container
-        spacing={8}
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-      >
-        <Grid item>
-          <Typography variant="h1">{title}</Typography>
+      <Grid container spacing={2} height="100vh">
+        <Grid item xs={3.8}></Grid>
+        <Grid item xs={4.4}>
+          <Grid
+            container
+            spacing={8}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            height="100vh"
+          >
+            <Grid item>
+              <Typography variant="h1">{title}</Typography>
+            </Grid>
+            <Grid item>
+              <Dice dragWrapperRef={dragWrapper} setTitle={setTitle} />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Dice dragWrapperRef={dragWrapper} setTitle={setTitle} />
-        </Grid>
+        <Grid item xs={3.8}></Grid>
       </Grid>
     </Container>
   )
