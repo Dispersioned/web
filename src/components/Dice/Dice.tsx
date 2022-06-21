50ecd70ab42251e7d6691d685ca86bd5004bcbf2
Dispersioned <dispersioned@gmail.com>
2022-06-21 10:51:31 +0300

    add dice grid

--- src/components/Dice/Dice.tsx
+++ src/components/Dice/Dice.tsx
@@ -1,14 +1,50 @@
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import './style.css'
 
 interface DiceProps {
   dragWrapperRef: React.MutableRefObject<null>
 }
 
+const DICE_SIZE = '370px'
+
+const Wrapper = styled.div`
+  position: relative;
+  width: ${DICE_SIZE};
+  height: ${DICE_SIZE};
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+`
+
+const GridLayer = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: grid;
+  grid-gap: 4px;
+  grid-template-columns: ${`repeat(3, calc(${DICE_SIZE} / 3 - 4px))`};
+  grid-template-rows: ${`repeat(3, calc(${DICE_SIZE} / 3 - 4px))`};
+`
+
+const Cell = styled.div`
+  background-color: #efefef;
+  border-radius: 5px;
+`
+
 const Dice: React.FC<DiceProps> = ({ dragWrapperRef }) => {
+  const [cells, setCells] = useState([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ])
+
   return (
-    <div className="dice-wrapper" ref={dragWrapperRef}>
+    <Wrapper ref={dragWrapperRef}>
+      <GridLayer>{cells.map((row, y) => row.map((_, x) => <Cell key={`${y}_${x}`} />))}</GridLayer>
       <motion.div
         className="dice"
         drag
@@ -16,7 +52,7 @@ const Dice: React.FC<DiceProps> = ({ dragWrapperRef }) => {
         whileDrag={{ backgroundColor: '#9f9f9f' }}
         dragTransition={{ bounceStiffness: 200, bounceDamping: 20 }}
       />
-    </div>
+    </Wrapper>
   )
 }
 export default Dice
