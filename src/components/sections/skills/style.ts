25fa558c4a3174b711234b3323dd40c24fc7443d
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-12-25 22:29:47 +0300

    feat: reinit project

--- src/components/sections/skills/style.ts
+++
@@ -1,8 +0,0 @@
-import { Chip, styled } from '@mui/material';
-
-export const SkillItem = styled(Chip)`
-  font-size: 18px;
-  background-color: var(--color-gray-100);
-  margin: 8px 0 0 10px;
-  border-radius: 5px;
-`;
